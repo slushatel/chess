bfd67dfd000afe1ceaa303dfa41e9ae3cb03b1a8
stas <rozhdennyinazeml@mail.ru>
2015-04-06 00:53:12 +0300

    app run spring, game service

---
+++ chess-service/src/main/java/com/javamonkeys/chess/webapi/GameService.java
@@ -0,0 +1,44 @@
+package com.javamonkeys.chess.webapi;
+
+///**
+// * Created by stas on 04.04.2015.
+// */
+//public class GameService {
+//}
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameService {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/chess")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
+
+class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

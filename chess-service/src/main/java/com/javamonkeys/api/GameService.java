9174ad1ed623c63a59ea334c720131115877d166
stas <rozhdennyinazeml@mail.ru>
2015-04-16 00:34:51 +0300

    Merge branch 'master' of https://github.com/alexserdukov/chess into alexserdukov-master
    Conflicts:
    	pom.xml

--- chess-service/src/main/java/com/javamonkeys/api/GameService.java
+++ chess-service/src/main/java/com/javamonkeys/api/GameService.java
@@ -5,9 +5,14 @@ import com.javamonkeys.dao.game.GameDao;
 import com.javamonkeys.dao.game.GameNotFoundException;
 import com.javamonkeys.dao.game.IGameDao;
 import com.javamonkeys.dao.user.User;
+import com.javamonkeys.dao.user.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 //import javax.inject.Inject;
 
@@ -19,26 +24,67 @@ public class GameService {
 //    @Autowired
 //    private IGameDao gameDao;
 
+//    @ResponseBody
+//    @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+//    public Game getGameExample() {
+//        return new Game(new User("email", "no password"));
+//    }
+
     @ResponseBody
-    @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public Game getGame() {
-        return new Game(new User("email","no password"));
+    @RequestMapping(value = "/user-games/{userId}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ArrayList<Game> getGamesByUserId(@RequestParam(value = "userId") int userId) {
+        GameDao gameDao = new GameDao();
+
+        //return gameDao.getGamesByUserId(userId);
+        return new ArrayList<Game>();
     }
 
     @ResponseBody
-    @RequestMapping(value = "/turn", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public boolean saveTurn(@RequestBody TurnJson turn) {
+    @RequestMapping(value = "/{gameId}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Game getGame(@RequestParam(value = "gameId") int gameId) {
+        GameDao gameDao = new GameDao();
+        return gameDao.getGame(gameId);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/new-game", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Game getNewGame(@RequestParam(value = "userToken") String userToken) {
+        UserDao userDao = new UserDao();
+        User user = userDao.getUserByToken(userToken);
+        return new Game(user);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/turn", method = RequestMethod.POST)
+    public ResponseEntity<resp<Game>> saveTurn(@RequestBody TurnJson turn) {
         GameDao gameDao = new GameDao();
 
         try {
-            gameDao.saveTurn(turn.id, turn.move);
-        } catch (GameNotFoundException e) {
-            return false;
+            //gameDao.saveTurn(turn.gameId, turn.fen, turn.pgn);
+            Game game = gameDao.getGame(turn.gameId);
+            resp<Game> resp1 = new resp<Game>(game, "");
+            return new ResponseEntity<resp<Game>>(resp1, HttpStatus.OK);
+//        } catch (GameNotFoundException e) {
+//            return null;
         } catch (Exception e) {
-            return false;
+            return null;
         }
 
-        return true;
+    }
+
+    class resp<T>{
+        T value;
+        String error;
+        resp(T value, String error){
+            this.value = value;
+            this.error = error;
+        }
+    }
+
+    class TurnJson {
+        int gameId;
+        String fen;
+        String pgn;
     }
 
     @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
@@ -57,9 +103,6 @@ public class GameService {
 
 }
 
-class TurnJson {
-    int id;
-    String move;
-}
+
 
 

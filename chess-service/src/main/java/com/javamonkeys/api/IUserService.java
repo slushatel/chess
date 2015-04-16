9ebe5c234efd1bd50fefb6ff12228bcc9143b3c7
afilippov-ua <afilippov.ua@gmail.com>
2015-04-16 21:54:48 +0300

    refactor UserService

--- chess-service/src/main/java/com/javamonkeys/api/IUserService.java
+++ chess-service/src/main/java/com/javamonkeys/api/IUserService.java
@@ -1,35 +1,21 @@
 package com.javamonkeys.api;
 
 import com.javamonkeys.dao.user.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Date;
 
 public interface IUserService {
 
-    public void register(String email, String password);
+    public void register(String authorization) throws UserAlreadyExistException, IncorrectUserCredentialsException;
 
-    public String login(String email, String password);
+    public String login(String authorization) throws IncorrectUserCredentialsException;
 
-    public void logout(User user);
+    public void logout(String token);
 
-    public User getUserByEmail(String email);
+    public User getUser(int id);
 
-    public User getUserByToken(String token);
+    public void deleteUser(int id) throws UserNotFoundException;
 
-    public User createUser(String eMail, String password,  UserAccessGroup userAccessGroup);
-
-    public User createUser(String eMail, String password, UserAccessGroup userAccessGroup, Date birthDate);
-
-    public void deleteUser(User user);
-
-    public void updateUser(User user);
-
-    public UserAccessGroup getUserAccessGroup(String name);
-
-    public UserAccessGroup createUserAccessGroup(String name, boolean isAdmin);
-
-    public void deleteUserAccessGroup(UserAccessGroup userAccessGroup);
+    public void updateUser(int id, User sourceUser) throws UserNotFoundException;
 
 }

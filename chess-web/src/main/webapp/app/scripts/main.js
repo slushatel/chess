cb3ace340045a46833131f9907a5ffe981e31e91
Alexandr Serdiukov <alexandr@zoomdata.com>
2015-05-03 18:38:52 +0300

    fixed template

--- chess-web/src/main/webapp/app/scripts/main.js
+++ chess-web/src/main/webapp/app/scripts/main.js
@@ -1,18 +1,18 @@
-/*global require*/
-'use strict';
+/*global ChessApp, $*/
 
-require.config({
-    shim: {
-    },
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/lodash/dist/lodash'
+
+window.ChessApp = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
     }
-});
+};
 
-require([
-    'backbone'
-], function (Backbone) {
-    Backbone.history.start();
+$(document).ready(function () {
+    'use strict';
+    ChessApp.init();
 });

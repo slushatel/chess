863013f607df3e1f78a82856c73c7be35897edd8
Alexandr Serdiukov <alexandr@zoomdata.com>
2015-05-03 21:27:37 +0300

    added backbone stubs

---
+++ chess-web/src/main/webapp/test/models/chess-app.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('ChessApp Model', function () {
+
+    beforeEach(function () {
+        this.ChessAppModel = new Webapp.Models.ChessApp();
+    });
+
+});
